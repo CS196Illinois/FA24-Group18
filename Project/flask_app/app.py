c807467ad1aff4da2b10d2bb1fc72146bf25abec
milocoder04011 <milocoder01@gmail.com>
2024-09-27 23:43:48 -0500

    Set up project template

---
+++ Project/flask_app/app.py
@@ -0,0 +1,2 @@
+# This is our main app file that will load all of our ML models and integrate the frontend and backend 
+# We will be using Flask/Django to integrate the frontend with the backend
---
+++ Project/flask_app/backend/.dummy.py
@@ -0,0 +1,1 @@
+# This is just a dummy file. We can delete this once we've added our actual notebook
